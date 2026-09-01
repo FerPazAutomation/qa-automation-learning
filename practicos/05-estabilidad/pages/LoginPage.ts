d5725ba0dbed4f6fff4e3ea8c45711c783c04540
FerPazAutomation <fernandollanespaz@gmail.com>
2026-09-01 13:14:12 -0300

    Fix the login locator to CI passed

--- practicos/05-estabilidad/pages/LoginPage.ts
+++ practicos/05-estabilidad/pages/LoginPage.ts
@@ -9,7 +9,7 @@ export class LoginPage {
   }
 
   async login(username: string, password: string) {
-    await this.page.getByRole("textbox", { name: "Username1" }).fill(username);
+    await this.page.getByRole("textbox", { name: "Username" }).fill(username);
     await this.page.getByRole("textbox", { name: "Password" }).fill(password);
     await this.page.getByRole("button", { name: "Login" }).click();
   }
